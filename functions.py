67ab5d870b99643bd14269abcfedd3f5dfe9c9aa
Damien Jeremiah <damien.jeremiah@altibox.no>
2021-05-16 14:12:19 +0200

    Added differential tool to check active nodes

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 __author__ = 'Damien Jeremiah'
 
+import filecmp, difflib
+
 from suds.client import Client as SudsClient
 
 # VIP adresse:
@@ -14,11 +16,34 @@ except Exception as e:
         print (repr(e))
 
 
+
+# This function will display the nodes that need to be added/deleted from Agama
+def difference_nodes_active():
+    # Path of first file1
+    file1 = "Differential_Tool/agama_today.txt"
+
+    # Path of second file2
+    file2 = "Differential_Tool/active_nodes_provision.txt"
+
+    with open(file1) as file_1:
+        file_1_text = file_1.readlines()
+
+    with open(file2) as file_2:
+        file_2_text = file_2.readlines()
+
+    # Find and print the diff:
+    for line in difflib.unified_diff(
+            file_1_text, file_2_text, fromfile='file1.txt',
+            tofile='file2.txt', lineterm=''):
+        print(line)
+    pass
+
+
 # This function will get all the nodes from monitoring
 def get_all_nodes():
     response = client.service.getAllProbeGroupNames()
     print (response)
-
+    pass
 
 # This function will be able to add a node to monitoring. Here we can add "Name" and "LPI Expression".
 #Example
@@ -27,10 +52,17 @@ def get_all_nodes():
 #
 def add_node():
     #response = client.service.addLpiProbeGroup("Damien5", "*Damien5*")
+    #response = client.service.addLpiProbeGroup("byk-hartevann1ar1", "*byk-hartevann1ar1*")
+    #response = client.service.addLpiProbeGroup("dabbmoi2ar1", "*dabbmoi2ar1*")
     #print (response)
+    pass
 
 
 # This function will delete a specific node
 def delete_node():
     #response = client.service.removeProbeGroup('Damien5')
+    #response = client.service.removeProbeGroup('dabbanasira1ar1')
+    #response = client.service.removeProbeGroup('dabbmoi1ar1')
     #print (response)
+    pass
+
--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import csv
 import functions
 
 
-#Function to retrieve all nodes
+### Function to retrieve all nodes
 #functions.get_all_nodes()
 
-#Function to add a node
+### Function to differntiate the nodes between provision and production
+functions.difference_nodes_active()
+
+### Function to add a node
 #functions.add_node()
 
-# Function to delete nodes.
+### Function to delete nodes.
 #functions.delete_node()
 
